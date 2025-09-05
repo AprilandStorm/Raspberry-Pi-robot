0a3f26d394041603705361b05668dbc8e3dd756d
AprilandStorm <2630185199@qq.com>
2025-09-05 09:14:45 +0800

    第二阶段

---
+++ cameraSystem/build/CMakeFiles/camera_server.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pi/project/cameraSystem/src/protocol.cpp" "CMakeFiles/camera_server.dir/src/protocol.cpp.o" "gcc" "CMakeFiles/camera_server.dir/src/protocol.cpp.o.d"
+  "/home/pi/project/cameraSystem/src/server.cpp" "CMakeFiles/camera_server.dir/src/server.cpp.o" "gcc" "CMakeFiles/camera_server.dir/src/server.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cameraSystem/build/CMakeFiles/camera_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/camera_server.dir/src/protocol.cpp.o"
+  "CMakeFiles/camera_server.dir/src/protocol.cpp.o.d"
+  "CMakeFiles/camera_server.dir/src/server.cpp.o"
+  "CMakeFiles/camera_server.dir/src/server.cpp.o.d"
+  "camera_server"
+  "camera_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/camera_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
